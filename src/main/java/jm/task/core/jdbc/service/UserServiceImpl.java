a76c831ace85d5a407f1368bb2a58a8ef311da75
AbuBakr <himik.2017@mail.ru>
2024-09-25 16:43:59 +0300

    pom fix

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -8,29 +8,29 @@ import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
-    private final UserDao dao = new UserDaoJDBCImpl();
+    private final UserDao userDao = new UserDaoJDBCImpl();
 
     public void createUsersTable() {
-        dao.createUsersTable();
+        userDao.createUsersTable();
     }
 
     public void dropUsersTable() {
-        dao.dropUsersTable();
+        userDao.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        dao.saveUser(name, lastName, age);
+        userDao.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        dao.removeUserById(id);
+        userDao.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return dao.getAllUsers();
+        return userDao.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        dao.cleanUsersTable();
+        userDao.cleanUsersTable();
     }
 }
